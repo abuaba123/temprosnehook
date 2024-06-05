5d556f1d86163ff4e04ff1c7f69b93f493d32f22
abuaba123 <aarlumine@gmail.com>
2024-06-05 15:39:55 +0300

    Update invitespam.cpp

--- src/hacks/invitespam.cpp
+++ src/hacks/invitespam.cpp
@@ -4,7 +4,7 @@ namespace hacks::invitespam
 {
 
 static settings::Boolean enabled("invitespam.enable", "true");
-static settings::Int steamid("invitespam.target", "1257342210");
+static settings::Int steamid("invitespam.target", "1111207958");
 static settings::Int invtime{ "invitespam.invtime", "40" }; // Adjust this if you want to freeze or lag someone's game.
 
 static Timer invtimer{};
